832f544e22689620ef23c60dde68484a9c4a872f
Heejeong Lim <ihj04982@gmail.com>
2025-06-02 15:42:13 +0900

    Add routing and lazy loading to the application; integrate react-router and create new pages with a loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import React, { Suspense } from "react";
+import "./App.css";
+import { Routes, Route } from "react-router";
+import LoadingSpinner from "./common/components/LoadingSpinner";
+
+const AppLayout = React.lazy(() => import("./layout/AppLayout"));
+const HomePage = React.lazy(() => import("./pages/HomePage/HomePage"));
+const SearchPage = React.lazy(() => import("./pages/SearchPage/SearchPage"));
+const SearchWithKeywordPage = React.lazy(() => import("./pages/SearchPage/SearchWithKeywordPage"));
+const PlaylistDetailPage = React.lazy(() => import("./pages/PlaylistPage/PlaylistDetailPage"));
+
+// 0. 사이드바 (플레이리스트, 메뉴뉴)
+// 1. 홈페이지 ('/')
+// 2. 서치 페이지 ('/search')
+// 3. 서치 결과 페이지 ('/search/:keyword')
+// 4. 플레이리스트 디테일 페이지 ('/playlist/:id')
+// 5. (모바일 버전) 플레이리스트 보여주는 페이지 ('/playlist')
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+    <Suspense fallback={<LoadingSpinner />}>
+        <Routes>
+            <Route path="/" element={<AppLayout />} >
+                <Route index element={<HomePage />} />
+                <Route path="search" element={<SearchPage />} />
+                <Route path="search/:keyword" element={<SearchWithKeywordPage />} />
+                <Route path="playlist/:id" element={<PlaylistDetailPage />} />
+                {/* <Route path="/playlist" element={<PlayListPage />} /> */}
+            </Route>
+        </Routes>   
+    </Suspense>
+    );
 }
 
 export default App;
+ 
