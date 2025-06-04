03d9b0a6e4313b93ea7733e2175866b69b87dcf2
Heejeong Lim <ihj04982@gmail.com>
2025-06-04 16:24:14 +0900

    Remove unused CreatePlaylistCard and CreatePlaylistButton components from AppLayout to streamline code and improve maintainability

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -51,26 +51,6 @@ const StyledNavLink = styled(NavLink)(({ theme }) => ({
   },
 }));
 
-const CreatePlaylistCard = styled(Card)({
-  padding: "20px",
-  borderRadius: "8px",
-});
-
-const CreatePlaylistButton = styled(Button)(({ theme }) => ({
-  backgroundColor: theme.palette.text.primary,
-  color: "black",
-  "&:hover": {
-    backgroundColor: theme.palette.text.secondary,
-  },
-  "&:active": {
-    backgroundColor: theme.palette.text.secondary,
-  },
-  fontWeight: 700,
-  padding: "6px 16px",
-  borderRadius: "30px",
-  marginTop: "20px",
-}));
-
 const AppLayout = () => {
   return (
     <Layout>
