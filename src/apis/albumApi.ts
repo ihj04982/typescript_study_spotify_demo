189352f7883fd9e0ac1f3b3aea9d6e1126f7d1d2
Heejeong Lim <ihj04982@gmail.com>
2025-06-09 13:36:27 +0900

    Refactor album API to utilize axios for fetching new releases, improving authorization handling with client credential token in request headers.

--- src/apis/albumApi.ts
+++ src/apis/albumApi.ts
@@ -1,10 +1,14 @@
 import { SPOTIFY_BASE_URL } from "../configs/commonConfig";
 import { IGetNewReleasesResponse } from "../models/album";
-import api from "../utils/api";
+import axios from "axios";
 
 export const getNewReleases = async (clientCredentialToken: string): Promise<IGetNewReleasesResponse> => {
   try {
-    const response = await api.get(`${SPOTIFY_BASE_URL}/browse/new-releases?limit=6`);
+    const response = await axios.get(`${SPOTIFY_BASE_URL}/browse/new-releases?limit=6`, {
+      headers: {
+        Authorization: `Bearer ${clientCredentialToken}`,
+      },
+    });
     return response.data;
   } catch (error) {
     throw new Error("Failed to get new releases");
